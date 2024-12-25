9585c0e88d703f40ffdd2109a6179f2ba256f40d
Erik Nielsen <erik@312development.com>
2024-12-25 07:20:13 -0500

    maint: ensuring dependencies are installed in correct order

--- packages/helpers/index.d.ts
+++ packages/helpers/index.d.ts
@@ -1,11 +1,11 @@
 export function formatSymbol(symbol: any): any;
 export function toDecimal(value: any, points?: number): number;
 export namespace constants {
-    let BUY: string;
-    let SELL: string;
-    let MARKET: string;
-    let GTC: string;
-    let BUY_TO_OPEN: string;
-    let SELL_TO_CLOSE: string;
-    let OPEN: string;
+  let BUY: string;
+  let SELL: string;
+  let MARKET: string;
+  let GTC: string;
+  let BUY_TO_OPEN: string;
+  let SELL_TO_CLOSE: string;
+  let OPEN: string;
 }
